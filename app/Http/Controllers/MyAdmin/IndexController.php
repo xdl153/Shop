8272be951cc51568a5a6c2ce23774d9e95e69cde
gujie <1030985069@qq.com>
2016-10-12 15:24:07 +0800

    3:22

--- app/Http/Controllers/MyAdmin/IndexController.php
+++ app/Http/Controllers/MyAdmin/IndexController.php
@@ -13,7 +13,7 @@ class IndexController extends Controller
 	// public function index()
 	// {
         // 
-        // helllow
+        // helllows
         // 
 	// 	return view("Adminstrator.index");
 	// }
