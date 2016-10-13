75f87eacb1d54bd2b07d5385b0c0aaaa414e75f3
Stone <Stone@STONEFC0E>
2016-10-13 09:08:20 +0800

    Stone Modified version 3.1

---
+++ app/Http/Controllers/MyShop/logoutController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\MyShop;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class logoutController extends Controller
+{
+    //清空登录session
+    public function logout()
+    {
+    	session()->forget("username");
+        return 'y';
+    }
+}
